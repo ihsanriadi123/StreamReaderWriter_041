05b6a47a201b2dc5e8539f2f74671996cdb46cbc
ihsan putra riadi <iputrariadi@gmail.com>
2023-06-21 15:44:35 +0700

    membuat catch membuat pengecualian dieksekusi

--- writterstream/writterstream.cpp
+++ writterstream/writterstream.cpp
@@ -6,7 +6,16 @@ int main() {
 		cout << "selamat belajar di Prodi TI UMY" << endl;
 		throw 0.5; //melemparkan sebuah integer maka
 		cout << "pernyataan tidak akan dieksekusi" << endl;
+
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "pengecualian akan dieksekusikan" << endl;
+	}
+	catch (...) {
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "default pengecualian dieksekusi" << endl;
 	}
 
-	catch (int a)
+	return 0;
 }
